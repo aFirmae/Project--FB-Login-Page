5b040747f46bf737f1f1fa8c93d015831561b9ec
afirmae <nilashis37@gmail.com>
2024-09-09 19:47:39 +0530

    Update form.js for form validation and add password toggle functionality

--- Form Validation/assets/js/form.js
+++ Form Validation/assets/js/form.js
@@ -64,3 +64,15 @@ loginForm.addEventListener('submit', (e) => {
         passwordInput.focus();
     }
 });
+
+const togglePassword = document.getElementById('togglePassword');
+const password = document.getElementById('password');
+
+togglePassword.addEventListener('click', function () {
+    // Toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // Toggle the eye icon
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+});
